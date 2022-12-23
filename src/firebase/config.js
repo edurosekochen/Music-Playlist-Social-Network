390ab3cd2f9eeda089ee21a4a75e242a144a3f65
Eduardo Rosenberg <105087418+edurosenberg@users.noreply.github.com>
2022-12-23 17:08:26 -0300

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, projectStorage, timestamp };
